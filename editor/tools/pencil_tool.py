12222f1723b80f1aec7b951142b5d8917d708e9c
slipfaith <128168725+Slipfaith@users.noreply.github.com>
2025-08-17 15:26:26 +0300

    refactor editor tools into modules

---
+++ editor/tools/pencil_tool.py
@@ -0,0 +1,26 @@
+from PySide6.QtCore import QPointF, QLineF
+from PySide6.QtWidgets import QGraphicsItem
+
+from .base_tool import BaseTool
+
+
+class PencilTool(BaseTool):
+    """Freehand drawing tool."""
+
+    def __init__(self, canvas):
+        super().__init__(canvas)
+        self._last_point = None
+
+    def press(self, pos: QPointF):
+        self._last_point = pos
+
+    def move(self, pos: QPointF):
+        if self._last_point is not None:
+            line = self.canvas.scene.addLine(QLineF(self._last_point, pos), self.canvas._pen)
+            line.setFlag(QGraphicsItem.ItemIsSelectable, True)
+            line.setFlag(QGraphicsItem.ItemIsMovable, True)
+            self.canvas._undo.append(line)
+            self._last_point = pos
+
+    def release(self, pos: QPointF):  # noqa: D401 - docs inherited
+        self._last_point = None
